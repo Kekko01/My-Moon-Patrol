f9a9048664dd1fe0f576ee7ddea905b8a56e4d1c
Francesco Ciociola <41902476+Kekko01@users.noreply.github.com>
2019-11-08 20:56:05 +0100

    First public Release

---
+++ classes.py
@@ -0,0 +1,397 @@
+#!/usr/bin/env python3
+'''
+@author  Francesco Ciociola - https://kekko01.altervista.org/blog/
+@from Michele Tomaiuolo - http://www.ce.unipr.it/people/tomamic
+@license This software is free - http://www.gnu.org/licenses/gpl.html
+'''
+
+from random import choice, randrange
+from actor import Actor, Arena
+
+
+class Rover(Actor):
+    def __init__(self, arena, pos, player):
+        self._x, self._y = pos
+        self._w, self._h = 31, 23
+        self._player = player
+        self._speed = 10
+        self._dx, self._dy = 0, 0
+        self._arena = arena
+        self._gravity=0.6
+        self._plane=55
+        arena.add(self)
+
+    def move(self):
+        arena_w, arena_h = self._arena.size()
+        self._arena_h=arena_h
+        self._dy+=self._gravity
+        self._y += self._dy
+        self._x += self._dx
+        if self._y < 0:
+            self._y = 0
+        elif self._y > arena_h - self._h-self._plane:
+            self._y = arena_h - self._h-self._plane
+
+#        self._x += self._dx
+#        if self._x < 0:
+#            self._x = 0
+#        elif self._x > arena_w - self._w:
+#            self._x = arena_w - self._w
+
+    def go_left(self):
+        if self._x>0:
+            self._dx-=3
+
+    def go_right(self):
+        if self._x+self._w<480:
+            self._dx+=3
+
+    def go_up(self):
+        if self._y + self._h >= self._arena_h - self._plane:
+            self._dx, self._dy = 0, -self._speed
+
+    def go_down(self):
+        self._dx, self._dy = 0, +self._speed
+
+    def stay(self):
+        self._dx, self._dy = 0, 0
+
+    def collide(self, other):
+        pass
+
+    def position(self):
+        return self._x, self._y, self._w, self._h
+
+    def symbol(self):
+        if self._player == 1:
+            if self._y + self._h <= self._arena_h - self._plane and self._dy < 0:
+                return 49, 150, self._w, self._h
+            elif self._y + self._h <= self._arena_h - self._plane and self._dy > 0 and self._y + self._h != self._arena_h - self._plane:
+                return 82 , 153 , self._w, self._h
+            return 249, 158, self._w, self._h
+        elif self._player == 2:
+            if self._y + self._h <= self._arena_h - self._plane and self._dy < 0:
+                return 47, 103, self._w, self._h
+            elif self._y + self._h <= self._arena_h - self._plane and self._dy > 0 and self._y + self._h != self._arena_h - self._plane:
+                return 80 , 104 , self._w, self._h
+            return 212, 158, self._w, self._h
+
+    def drop(self):
+        if self._y<self._arena_h:
+            self._dy+=self._gravity/2
+            self._y += self._dy
+
+    def drop_symbol(arg):
+        return 167,100,41,34
+
+    def x_position(self):
+        return self._x+self._w
+
+    def y_position(self):
+        return self._y+self._h/2
+
+class Background:
+    """docstring for Background."""
+
+    def __init__(self, pos, speed, image, dimensions):
+        self._image_x,self._image_y=image
+        self._startpoint=pos[0]
+        self._x, self._y = pos
+        self._w, self._h = dimensions
+        self._speed = speed
+        self._dx = self._speed
+
+    def move(self):
+        if self._x+self._w<=self._startpoint:
+            self._x=self._startpoint
+        self._x -= self._dx
+
+    def position(self):
+        return self._x, self._y, self._w, self._h
+
+    def collide(self, other):
+        pass
+
+    def symbol(self):
+        return self._image_x, self._image_y, self._w, self._h
+
+class Hole(Arena):
+    """docstring for Hole."""
+
+    def __init__(self, arena, image, pos):
+        self._x,self._y=480,300
+        self._w,self._h=22,27
+        self._image_x,self._image_y=image
+        self._speed = 6
+        self._dx = self._speed
+        arena.add(self)
+
+    def move(self):
+        self._x -= self._dx
+
+    def position(self):
+        return self._x, self._y, self._w, self._h
+
+    def collide(self,other):
+        x,y,w,h=other.position()
+        if x + w > self._x and y + h > self._y and x < self._x + self._w:
+            return True
+        else:
+            return False
+
+    def symbol(self):
+        return self._image_x, self._image_y, self._w, self._h
+
+    def x_position(self):
+        return self._x+self._w
+
+class Hill(Arena):
+    """docstring for Hill."""
+
+    def __init__(self, arena, image, dimension,life):
+        self._w,self._h=dimension
+        self._x,self._y=480,305-self._h
+        self._image_x,self._image_y=image
+        self._speed = 6
+        self._dx = self._speed
+        self._life=life
+        arena.add(self)
+
+    def move(self):
+        self._x -= self._dx
+
+    def position(self):
+        return self._x, self._y, self._w, self._h
+
+    def collide(self,other):
+        x,y,w,h=other.position()
+        if isinstance(other,Rover):
+            if y + h >= self._y and self._x <= x + w and x< self._x + self._w:
+                return True
+            else:
+                return False
+        elif isinstance(other,Bullet):
+            if x + w >= self._x and y >= self._y :
+                return True
+            else:
+                return False
+
+    def symbol(self):
+        return self._image_x, self._image_y, self._w, self._h
+
+    def x_position(self):
+        return self._x+self._w
+
+    def get_life(self):
+        return self._life
+
+    def take_life(self):
+        self._life-=1
+
+class Alien(Arena):
+    """docstring for Hole."""
+
+    def __init__(self, arena):
+        self._x,self._y=480,100
+        self._w,self._h=17,10
+        self._image_x,self._image_y=67,229
+        self._speed = 3
+        self._dx = self._speed
+        arena.add(self)
+
+    def move(self):
+        self._dy = choice((-1,-0.5,0,0.5,1))
+        self._y += self._dy
+        self._x -= self._dx
+
+    def position(self):
+        return self._x, self._y, self._w, self._h
+
+    def collide(self,other):
+        x,y,w,h=other.position()
+        if isinstance(other,Bullet):
+            if y + h >= self._y and self._x <= x + w and x< self._x + self._w:
+                return True
+            else:
+                return False
+
+    def symbol(self):
+        return self._image_x, self._image_y, self._w, self._h
+
+    def x_position(self):
+        return self._x+self._w
+
+    def y_position(self):
+        return self._y+self._h
+
+    def delete(self):
+        self._x=-100
+
+
+class AlienBullet(Arena):
+    """docstring for AlienBullet."""
+
+    def __init__(self, arena, pos):
+        self._x,self._y=pos
+        self._x-=3
+        self._speed=5
+        self._w,self._h=5,6
+        self._image_x,self._image_y=213,231
+        arena.add(self)
+
+    def move(self):
+        self._y+=self._speed
+
+    def position(self):
+        return self._x, self._y, self._w, self._h
+
+    def collide(self,other):
+        x,y,w,h=other.position()
+        if isinstance(other,Rover):
+            if x<=self._x<=x+w and self._y+self._h>=y:
+                return True
+            else:
+                return False
+
+    def symbol(self):
+        return self._image_x, self._image_y, self._w, self._h
+
+    def x_position(self):
+        return self._x
+
+    def y_position(self):
+        return self._y
+
+class Bullet(Arena):
+    """docstring for Bullet."""
+
+    def __init__(self, arena, x, y, dir):
+        self._x,self._y=x,y
+        self._w,self._h=8,8
+        self._image_x,self._image_y=269,142
+        self._speed = 8
+        if dir==0:
+            self._dx = self._speed
+            self._dy = 0
+        elif dir==1:
+            self._dy = self._speed
+            self._dx = 0
+        arena.add(self)
+
+    def collide(self, other):
+        pass
+#        x,y,w,h=other.position()
+#        if self._x + self._w >= x and self._y >= y :
+#            return True
+#        else:
+#            return False
+
+    def move(self):
+        self._x += self._dx
+        self._y -= self._dy
+
+    def position(self):
+        return self._x, self._y, self._w, self._h
+
+    def symbol(self):
+        return self._image_x, self._image_y, self._w, self._h
+
+    def x_position(self):
+        return self._x + self._w
+
+    def y_position(self):
+        return self._y + self._h
+
+class Robot(Arena):
+    """docstring for Hole."""
+
+    def __init__(self, arena):
+        self._x,self._y=480,290
+        self._w,self._h=16,16
+        self._image_x,self._image_y=109,246
+        self._speed = 4
+        self._dx = self._speed
+        arena.add(self)
+
+    def move(self):
+        self._x -= self._dx
+
+    def position(self):
+        return self._x, self._y, self._w, self._h
+
+    def collide(self,other):
+        x,y,w,h=other.position()
+        if isinstance(other,Bullet):
+            if self._y + self._h >= y >= self._y and self._x <= x + w <= self._x + self._w:
+                return True
+        elif isinstance(other,Rover):
+            if self._y < y + h and self._x <= x + w <= self._x + self._w:
+                return True
+        return False
+
+    def symbol(self):
+        return self._image_x, self._image_y, self._w, self._h
+
+    def x_position(self):
+        return self._x+self._w
+
+    def y_position(self):
+        return self._y+self._h
+
+    def delete(self):
+        self._x=-100
+
+class RobotBullet(Arena):
+    """docstring for AlienBullet."""
+
+    def __init__(self, arena, pos):
+        self._x,self._y=pos
+        self._y-=15
+        self._x-=15
+        self._speed=7
+        self._w,self._h=4,1
+        self._image_x,self._image_y=94,248
+        arena.add(self)
+
+    def move(self):
+        self._x-=self._speed
+
+    def position(self):
+        return self._x, self._y, self._w, self._h
+
+    def collide(self,other):
+        x,y,w,h=other.position()
+        if isinstance(other,Rover):
+            if x<=self._x<=x+w and y+h>=self._y:
+                return True
+            else:
+                return False
+
+    def symbol(self):
+        return self._image_x, self._image_y, self._w, self._h
+
+    def x_position(self):
+        return self._x
+
+    def y_position(self):
+        return self._y
+
+##class BounceGame:
+##    def __init__(self):
+##        self._arena = Arena(320, 240)
+##        Ball(self._arena, 40, 80)
+##        Ball(self._arena, 80, 40)
+##        Ghost(self._arena, 120, 80)
+##        self._hero = Turtle(self._arena, 80, 80)
+##
+##    def arena(self) -> Arena:
+##        return self._arena
+##
+##    def hero(self) -> Turtle:
+##        return self._hero
+
+
+def print_arena(arena):
+    pass
+#    for a in arena.actors():
+#        print(type(a).__name__, '@', a.position())
---
+++ moonpatrol.py
@@ -0,0 +1,242 @@
+#!/usr/bin/env python3
+'''
+@author  Francesco Ciociola - https://kekko01.altervista.org/blog/
+@from Michele Tomaiuolo - http://www.ce.unipr.it/people/tomamic
+@license This software is free - http://www.gnu.org/licenses/gpl.html
+'''
+
+import g2d, random
+from classes import Arena, Rover, Background, Hole, Hill, Bullet, Alien, AlienBullet, Robot, RobotBullet
+
+arena = Arena((480, 360))
+#b1 = Ball(arena, (40, 80))
+#b2 = Ball(arena, (80, 40))
+#g = Ghost(arena, (120, 80))
+ask=False
+sound=g2d.load_audio("assets/sound.mp3")
+sprites = g2d.load_image("assets/moon-patrol.png")
+backgroundsimage=g2d.load_image("assets/moon-patrol-bg.png")
+backgrounds=[Background((0,0),2,(0,0),(510,257)),Background((510,0),2,(0,0),(510,257)),Background((0,180),4,(0,384),(510,127)),Background((510,180),4,(0,384),(510,127)),Background((0,300),6,(0,515),(510,127)),Background((510,300),6,(0,515),(510,127))]
+holes=[]
+hills=[]
+bullets=[]
+aliens=[]
+aliensbullets=[]
+robots=[]
+robotsbullets=[]
+spawntimer=0
+game=True
+writing=False
+with open("assets/score","r") as target:
+    highscore=target.read()
+score=0
+level=1
+def tick():
+    global spawntimer,game,score,writing,level,ask,multiplayer,rover,rover1
+    if game:
+        if not ask:
+            multiplayer=int(g2d.prompt("Multiplayer? (1=yes, 0 or other=no)"))
+            if multiplayer!=1:
+                multiplayer=False
+            elif multiplayer==1:
+                multiplayer=True
+            rover = Rover(arena, (60, 300), 1)
+            if multiplayer:
+                rover1 = Rover(arena, (100, 300), 2)
+            ask=True
+        g2d.play_audio(sound,True)
+        if score % 20 == 0 and score!=0:
+            level = score // 20 + 1
+            score+=2
+        if g2d.key_pressed("ArrowUp"):
+            rover.go_up()
+        if g2d.key_pressed("ArrowRight"):
+            rover.go_right()
+    #    if g2d.key_pressed("ArrowDown"):
+    #        rover.go_down()
+        if g2d.key_pressed("ArrowLeft"):
+            rover.go_left()
+        if (g2d.key_released("ArrowUp") or
+              g2d.key_released("ArrowRight") or
+              g2d.key_released("ArrowDown") or
+              g2d.key_released("ArrowLeft")):
+            rover.stay()
+
+        if multiplayer:
+            if g2d.key_pressed("w"):
+                rover1.go_up()
+            if g2d.key_pressed("d"):
+                rover1.go_right()
+        #    if g2d.key_pressed("ArrowDown"):
+        #        rover.go_down()
+            if g2d.key_pressed("a"):
+                rover1.go_left()
+            if (g2d.key_released("w") or
+                  g2d.key_released("d") or
+                  g2d.key_released("s") or
+                  g2d.key_released("a")):
+                rover1.stay()
+
+        arena.move_all()  # Game logic
+
+        g2d.clear_canvas()
+
+        if g2d.key_pressed("Spacebar"):
+            bullets.append(Bullet(arena, rover.x_position(), rover.y_position(), 0))
+            bullets.append(Bullet(arena, rover.x_position(), rover.y_position(), 1))
+        if g2d.key_pressed("z") and multiplayer:
+            bullets.append(Bullet(arena, rover1.x_position(), rover1.y_position(), 0))
+            bullets.append(Bullet(arena, rover1.x_position(), rover1.y_position(), 1))
+
+        for a in backgrounds:
+            a.move()
+            g2d.draw_image_clip(backgroundsimage, a.symbol(), a.position())
+
+        for a in arena.actors():
+            if a.symbol() != (0, 0, 0, 0):
+                g2d.draw_image_clip(sprites, a.symbol(), a.position())
+            else:
+                g2d.fill_rect(a.position())
+
+        if spawntimer==0:
+            if random.randint(0,20)==0:
+                holes.append(Hole(arena, (131,167), (480,300)))
+                spawntimer=120/level
+            elif random.randint(0,20)==0:
+                holes.append(Hole(arena, (159,167), (480,300)))
+                spawntimer=120/level
+            elif random.randint(0,20)==0:
+                hills.append(Hill(arena,(80,203),(13,16),1))
+                spawntimer=120/level
+            elif random.randint(0,20)==0:
+                hills.append(Hill(arena,(112,199),(13,16),2))
+                spawntimer=120/level
+            elif random.randint(0,10)==0:
+                aliens.append(Alien(arena))
+                spawntimer=120/level
+            elif random.randint(0,10)==0:
+                robots.append(Robot(arena))
+                spawntimer=120/level
+        if spawntimer>0:
+            spawntimer-=1
+
+        for a in holes:
+            if a.x_position()<=0:
+                holes.remove(a)
+            if a.collide(rover):
+                game=False
+            if multiplayer:
+                if a.collide(rover1):
+                    game=False
+
+        for a in hills:
+            if a.x_position()<=0:
+                hills.remove(a)
+                arena.remove(a)
+            if a.collide(rover):
+                game=False
+            if multiplayer:
+                if a.collide(rover1):
+                    game=False
+            for i in bullets:
+                if a.collide(i):
+                    if a.get_life()==2:
+                        a.take_life()
+                        bullets.remove(i)
+                        arena.remove(i)
+                        score+=1
+                    else:
+                        bullets.remove(i)
+                        hills.remove(a)
+                        arena.remove(i)
+                        arena.remove(a)
+                        score+=1
+        for a in bullets:
+            if a.x_position()>480:
+                bullets.remove(a)
+                arena.remove(a)
+            if a.y_position()<0:
+                bullets.remove(a)
+                arena.remove(a)
+        for a in aliens:
+            if a.x_position()<=0:
+                aliens.remove(a)
+                arena.remove(a)
+            if random.randint(0,60)==0:
+                aliensbullets.append(AlienBullet(arena, (a.x_position(),a.y_position())))
+            for i in bullets:
+                if a.collide(i):
+                    bullets.remove(i)
+                    a.delete()
+                    #aliens.remove(a)
+                    arena.remove(i)
+                    arena.remove(a)
+                    score+=1
+        for a in robots:
+            if a.x_position()<=0:
+                robots.remove(a)
+                arena.remove(a)
+            if random.randint(0,60)==0:
+                robotsbullets.append(RobotBullet(arena, (a.x_position(),a.y_position())))
+            for i in bullets:
+                if a.collide(i):
+                    bullets.remove(i)
+                    a.delete()
+                    #aliens.remove(a)
+                    arena.remove(i)
+                    arena.remove(a)
+                    score+=1
+            if a.collide(rover):
+                game=False
+            if multiplayer:
+                if a.collide(rover1):
+                    game=False
+        for a in aliensbullets:
+            if a.y_position()>300:
+                if random.randint(0,3)==0:
+                    holes.append(Hole(arena, (131,167), (a.x_position(),300)))
+                    aliensbullets.remove(a)
+                    arena.remove(a)
+            if a.collide(rover):
+                game=False
+            if multiplayer:
+                if a.collide(rover1):
+                    game=False
+
+        for a in robotsbullets:
+            if a.y_position()<=0:
+                aliensbullets.remove(a)
+                arena.remove(a)
+            if a.collide(rover):
+                game=False
+            if multiplayer:
+                if a.collide(rover1):
+                    game=False
+
+    else:
+        g2d.pause_audio(sound)
+        g2d.set_color((255,0,0))
+        g2d.draw_text("Game Over",(110,170),50)
+        g2d.draw_image_clip(sprites, rover.drop_symbol(), rover.position())
+        rover.drop()
+        rover.position()
+        if multiplayer:
+            g2d.draw_image_clip(sprites, rover1.drop_symbol(), rover1.position())
+            rover1.drop()
+            rover1.position()
+        if not writing and score > int(highscore):
+            with open("assets/score","w") as target:
+                target.write(str(score))
+            writing=True
+    g2d.draw_text("Highscore:", (20, 20), 30)
+    g2d.draw_text(highscore, (170, 10), 40)
+    g2d.draw_text("Score:", (330, 20), 30)
+    g2d.draw_text(str(score), (420, 10), 40)
+    g2d.draw_text("Level:", (20, 70), 30)
+    g2d.draw_text(str(level), (100, 60), 40)
+
+def main():
+    g2d.init_canvas(arena.size())
+    g2d.main_loop(tick)
+
+main()
